4c0d7927d34dfe7029269392627e1c7e9a8c551e
rockcm <rockcm@etsu.edu>
2024-11-14 20:57:46 -0500

    fixed display issues, added gap and grid display attribute to css. changed gridRows and gridCols to fit to frame. Changed class and id names to better match what they do. game-board -> Frame, game -> game-board.

--- Mineswepper.js
+++ Mineswepper.js
@@ -1,5 +1,5 @@
 // select elements
-const gameBoard = document.getElementById('game-board');
+const game = document.getElementById('game-board');
 
 // game settings
 const gridRows = 10; // 10x10 grid
@@ -8,10 +8,11 @@ const gridCols = 10;
 // function to initialize the game grid (just printing the grid)
 function initializeGame() 
 {
-  gameBoard.innerHTML = '';  // clear the game board
+  game.innerHTML = '';  // clear the game board
 
   // set the grid template for the CSS grid layout - can be altered later if needed for dynamic game board size
-  gameBoard.style.gridTemplateColumns = `repeat(${gridCols}, 42px)`;
+  game.style.gridTemplateColumns = `repeat(${gridCols}, 39px)`; 
+  game.style.gridTemplateRows = `repeat(${gridRows}, 39px)`;
 
   // create the grid cells in the DOM using nested for loops 
   for (let row = 0; row < gridRows; row++) 
@@ -24,14 +25,14 @@ function initializeGame()
       console.log(row); // testing log statement can be removed 
       cell.dataset.col = col;
       console.log(col); // testing log statement can be removed 
-      gameBoard.appendChild(cell);
+      game.appendChild(cell);
     }
     
   }
 
 }
 
-// call initializeGameBoard to create the grid
+// call initializegame to create the grid
 initializeGame();
 
 
